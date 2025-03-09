1ef777a531588786358433131f3da0898d8eefe8
https://github.com/SathwikGuduguntla <guduguntlasathwik@gmail.com>
2025-03-09 19:43:53 +0530

    2ed day

---
+++ assigment.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int num = 0;
+    int num1 = 0, num2 = 0, num3 = 1, num4 = 1;
+    num1 += 1;
+    num2 -= 1;
+    num3 /= 2;
+    num4 %= 2;
+    cout << num1 << endl;
+    cout << num2 << endl;
+    cout << num3 << endl;
+    cout << num4 << endl;
+    return 0;
+} 
