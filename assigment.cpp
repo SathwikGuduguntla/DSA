069238fc40a6cb6b1a5cf289de138d06edafd152
https://github.com/SathwikGuduguntla <guduguntlasathwik@gmail.com>
2025-06-26 01:10:30 +0530

    first commit

--- assigment.cpp
+++ assigment.cpp
@@ -1,15 +1,56 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main() {
-    int num = 0;
-    int num1 = 0, num2 = 0, num3 = 1, num4 = 1;
-    num1 += 1;
-    num2 -= 1;
-    num3 /= 2;
-    num4 %= 2;
-    cout << num1 << endl;
-    cout << num2 << endl;
-    cout << num3 << endl;
-    cout << num4 << endl;
-    return 0;
-} 
+    int a , b ;
+    cout << "enter the numbers to swap";
+    cin >> a >> b;
+    cout << " a is " << a << "b id " << b <<endl;
+    a= a+b;
+    b = a-b;
+    a = a -b;
+    cout << " after swapping a is " << a << " and b is " << b << endl; 
+   
+    cout << "entwer the numbers" ;
+    cin >> a >> b;
+    cout << "the addition of the a and b is " << a + b  << endl;
+    cout << "the subtraction of the a and b is " << a - b  << endl;
+    cout << "the multiplication of the a and b is " << a * b  << endl;
+    cout << "the division of the a and b is " << a / b  << endl;
+    cout << "the modulus of the a and b is " << a % b  << endl;
+    cout << "the increment of the a is " << ++a << endl;
+    cout << "the decrement of the a is " << --a << endl; 
+
+    if ( a % 2 == 0 || b % 2 == 0   ) {
+        cout << "the number is even  " << endl;
+    } else {
+        cout << "the number is odd " << endl;   
+    } 
+     
+    int n , area , r , pi = 3.12; 
+    cout << " enter the number ";
+    cin >> n >> r;
+    area =  pi*n* r^2 ;
+    cout << "the area of the triangle is " << area << endl; 
+    
+    int  sum;
+    cout << "enter the numner N natural numbers";
+    cin >> n;
+    sum = (n * (n + 1)) / 2;
+    cout << "the sum of the first " << n << " natural numbers is " << sum << endl;
+     
+    char d;
+    cout << " enter the character to find the ascil value ";
+    cin >> d;
+    cout<< " the value of the ascill value of the character " << d << " is " << int(d) << endl;
+
+    int minutes , hours ;
+    cout << " enter the minutes to convert into hours and minutes ";
+    cin >> minutes;
+    hours = minutes / 60;
+    minutes = minutes % 60;
+    cout << "the time in hours and minutes is " << hours << " hours and "
+            << minutes << " minutes" << endl;
+
+
+
+}  
--- misc.cpp
+++ misc.cpp
@@ -1,26 +1,58 @@
 #include <iostream>
 using namespace std;
+int main()
+{
+    cout << "hello world" << endl;
+    int age;
+    cout << "enter your age";
+    cin >> age;
+    cout << "your age is" << age << endl;
+    string name, college;
+    cout << "enter your name ";
+    cin >> name;
+    cout << "your name is " << name << endl;
+    cout << "your name is " << name << " and your age is " << age << endl;
+    cout << " enter your college name ";
+    cin >> college;
+    cout << "your college name is " << college << endl;
 
-int main() {
-    int num = 4;
-    cout <<sizeof (num) << endl;
-    char name = 'a';
-   cout <<sizeof (name) << endl;
-    bool flg;
-    flg = (name == 'a') ? true : false;  
-    cout << flg << endl; 
+    /* Data types
+    int
+    float
+    double
+    char
+    string
+    bool
+    */
+    int x = 0;
+    cout << "enter a nunber ";
+    cin >> x;
+    cout << "the number you entered is  " << x << endl;
+    float pi = 3.12;
+    cout << "the value of the pi is " << pi << endl;
+    char initial = 'A';
+    cout << "the initial is " << initial << endl;
+    bool ispass = true;
+    cout << "thew value of the pass is " << ispass << endl;
 
-    cout << (void*)&name << endl; 
+    int a, b;
+    cout << "enter two numbers to add";
+    cin >> a >> b;
+    cout << "the sum of the two numbers is " << a + b << endl;
 
-   int  a = 6;
-    cout << (a++)<<endl;
-    int b =a+5;
-    cout << (b) <<endl; 
-    cout << (--b) <<endl;
-    cout << (b--) <<endl;
-     char ch = 'pqr';
-    cout <<(4+2 "ch") <<endl;
-    cout<< ("pqr" + 4 + 2);
+    double celsius, fehrenheit;
+    cout << " ennter the tempature in celsius ";
+    cin >> celsius;
+    fehrenheit = (celsius * 9.0 / 5.0) + 32.0;
+    cout << "the temparture in fehranherit is " << fehrenheit << endl;
+
+    int num1, num2, temp;
+    cout << " enter the two numbers to swap ";
+    cin >> num1 >> num2;
+    temp = num1;
+    num1 = num2;
+    num2 = temp;
+    cout << "the first number is " << num1 << " and the second number is " << num2 << endl;
 
     return 0;
 }
