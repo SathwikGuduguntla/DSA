3fc10b6522a8a02886a8a782034a1b7c3270a743
https://github.com/SathwikGuduguntla <guduguntlasathwik@gmail.com>
2025-07-09 00:00:06 +0530

    commited

--- patterns.cpp
+++ patterns.cpp
@@ -8,23 +8,31 @@ print '*'
 
 
 */
-
-#include <iostream>
+#include <iostrea>
 using namespace std;
-void printl(int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j <=i; j++)
-        {
-            cout << "*";
+
+void print7(int n) {
+    for(int i=0;i<n n;i++) {
+        for(int j=0;j<n-i-1;j++) {
+              cout << "";
+
         }
-        cout << endl;
+        for(int j=0;j<=2*i+1;j++){
+            cout << "*";
+        } 
+        for(int j=0;j<=n-i-1;j++){
+            cout << "";
+        }
+        cout <<endl;
     }
 }
-int main()
-{
-    int n;
-    cin >> n;
-    printl(n);
+int main() {
+    int t;
+    cin >>t;
+    for(int i=0;i<t;i++) {
+        int n;
+        cin >>n;
+        print7(n);
+    }
 }
+    
